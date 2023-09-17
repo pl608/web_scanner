0e0f4e7572b1eeaa197a1a47764cb7a22848475b
pl608 <78400949+pl608@users.noreply.github.com>
2023-09-17 11:47:02 -0700

    Update database_module.py

--- database_module.py
+++ database_module.py
@@ -2,7 +2,7 @@ from requests import get
 from json import loads, dumps
 from pickle import loads as load_, dumps as dump_    
 class Database():
-    db_url = ''
+    db_url = 'https://simpledatabase.pl608.repl.co/'
     def __init__(self):
         self.sid = loads(get(f'{self.db_url}').text).get('sid')
     def save(self, key:str, value:str):
@@ -17,4 +17,13 @@ class Database():
         return resp
     def __getitem__(self, key):
         return get(key)
+    def append(self, key):
+        self.save(key, None)
+    def check_in(self, key):
+        resp = loads(get(f'{self.db_url}/?sid={self.sid}&key={key}&cmd=in').text)
+        if type(resp)==bool:
+            return resp
+        else:
+            raise KeyError(resp.get('error', 'No Error Returned')) 
+
     
