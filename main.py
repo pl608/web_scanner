005ba918d4bc9198fa87280ab18392aee59468a3
pl608 <78400949+pl608@users.noreply.github.com>
2023-09-17 12:09:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -30,18 +30,17 @@ def scan(url, parent):
             sleep(.5)
             new_url = link.attrs.get('href')
             if new_url != None:
-
                 if new_url.startswith('http'):
                     if  domains.check_in(get_domain(new_url)) == False:
-                        domains.append(get_domain(new_url))
+                        domains.save(get_domain(new_url), True)
+                        #print(get_domain(new_url))
                         Thread(target=scan, args=(new_url, url)).start()
                 else:
                     url_ = f"{url}{new_url}"
                     Thread(target=scan, args=(url_, url)).start()
             #print(link.attrs.get('href'))
     except Exception as e:
-        #fails.append((url, str(e)))
-        pass
+        fails.append((url, str(e)))
 def thread_handler():
     global thread_que
     while True:
